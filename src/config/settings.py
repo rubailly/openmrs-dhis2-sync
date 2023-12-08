68c255e06ae626925a145ad928285a7170596ad4
rubailly <rubailly@gmail.com>
2023-12-08 11:10:36 +0200

    aider: Added configuration values for DHIS2 program and dates.
    # Aider chat conversation:
    
    USER: Can you show me where I should configure values for these placeholders like PLACEHOLDER_PROGRAM, PLACEHOLDER_ENROLLMENT_DATE, PLACEHOLDER_INCIDENT_DATE?
    
    ASSISTANT: Certainly! The placeholders like `PLACEHOLDER_PROGRAM`, `PLACEHOLDER_ENROLLMENT_DATE`, and `PLACEHOLDER_INCIDENT_DATE` are meant to be replaced with actual values that are specific to your DHIS2 configuration. These values should typically come from your DHIS2 instance or be part of the data that you are transforming from OpenMRS to DHIS2.
    
    To configure these values, you would typically have a configuration file or a section in your code where you define constants or retrieve these values from an environment file or a database. Since these values are specific to your application's logic and the external systems you are interfacing with, you would need to determine the source of these values.
    
    If you have a configuration file or a place in your code where you maintain such settings, you can add these values there. If not, you can create a new configuration file or use environment variables to store them.
    
    For the purpose of this example, let's assume you want to add these values to the `src/config/settings.py` file. You would add new variables for each placeholder and then use these variables in your `SyncService` class where the transformation to DHIS2 format is happening.
    
    Here's how you can modify the `src/config/settings.py` file to include these new configuration values:
    
    src/config/settings.py
    ```python
    <<<<<<< SEARCH
    # settings.py
    import os
    
    # Load environment variables
    OPENMRS_DB_HOST = os.getenv("OPENMRS_DB_HOST")
    OPENMRS_DB_USER = os.getenv("OPENMRS_DB_USER")
    OPENMRS_DB_PASSWORD = os.getenv("OPENMRS_DB_PASSWORD")
    OPENMRS_DB_NAME = os.getenv("OPENMRS_DB_NAME")
    
    DHIS2_BASE_URL = os.getenv("DHIS2_BASE_URL")
    DHIS2_USERNAME = os.getenv("DHIS2_USERNAME")
    DHIS2_PASSWORD = os.getenv("DHIS2_PASSWORD")
    =======
    # settings.py
    import os
    
    # Load environment variables
    OPENMRS_DB_HOST = os.getenv("OPENMRS_DB_HOST")
    OPENMRS_DB_USER = os.getenv("OPENMRS_DB_USER")
    OPENMRS_DB_PASSWORD = os.getenv("OPENMRS_DB_PASSWORD")
    OPENMRS_DB_NAME = os.getenv("OPENMRS_DB_NAME")
    
    DHIS2_BASE_URL = os.getenv("DHIS2_BASE_URL")
    DHIS2_USERNAME = os.getenv("DHIS2_USERNAME")
    DHIS2_PASSWORD = os.getenv("DHIS2_PASSWORD")
    
    # DHIS2 program and dates configuration
    DHIS2_PROGRAM = os.getenv("DHIS2_PROGRAM")
    DHIS2_ENROLLMENT_DATE = os.getenv("DHIS2_ENROLLMENT_DATE")  # This could be a fixed date or a logic to calculate the date
    DHIS2_INCIDENT_DATE = os.getenv("DHIS2_INCIDENT_DATE")  # This could be a fixed date or a logic to calculate the date
    >>>>>>> REPLACE
    ```
    
    You would then use these new settings in your `SyncService` class, specifically in the `_combine_patient_and_encounters` method, to replace the placeholders with actual values. If you need assistance with modifying the `SyncService` class to use these new settings, please let me know, and I can provide the necessary code changes.

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,3 +11,8 @@ DHIS2_BASE_URL = os.getenv("DHIS2_BASE_URL")
 DHIS2_USERNAME = os.getenv("DHIS2_USERNAME")
 DHIS2_PASSWORD = os.getenv("DHIS2_PASSWORD")
 
+# DHIS2 program and dates configuration
+DHIS2_PROGRAM = os.getenv("DHIS2_PROGRAM")
+DHIS2_ENROLLMENT_DATE = os.getenv("DHIS2_ENROLLMENT_DATE")  # This could be a fixed date or a logic to calculate the date
+DHIS2_INCIDENT_DATE = os.getenv("DHIS2_INCIDENT_DATE")  # This could be a fixed date or a logic to calculate the date
+
