1f3e0635b3aa5964319e4a685ccaee6c41d3f645
rubailly <rubailly@gmail.com>
2023-12-14 15:07:30 +0200

    aider: Fix the `process_patient_files` method in `src/connectors/dhis2_connector.py` to correctly extract the tracked entity instance data and events from the patient data.
    # Aider chat conversation:
    
    USER: process_patient_files in `src/connectors/dhis2_connector.py` will not work because tracked_entity_instance_data = patient_data.get('trackedEntityInstance') will fail as we don't have trackedEntityInstance in the json. Our json is like "{ "trackedEntityType": "j9TllKXZ3jb", "orgUnit": "RHmp1yicPZW", "attributes": [ { "attribute": "b6YzTWXj4nF", "value": "2fb47edd-bf22-4f75-aac1-b5fa47f1284e" }, { "attribute": "alSROc4vnA7", "value": "Izayi" }, { "attribute": "LKm1eTNhn4G", "value": "Manirafasha" }, { "attribute": "CW3Ai3BrfVy", "value": "0783325301" }, { "attribute": "Yto0ghm8kOE", "value": "646" }, { "attribute": "lkHesHl0PcX", "value": "646" }, { "attribute": "hyWq5PzG0rw", "value": "West" }, { "attribute": "Kv01wmCxVH6", "value": "Rusizi / Western Province/Uburengerazuba" }, { "attribute": "boAQ2CHQffk", "value": "Kamembe / Rusizi" }, { "attribute": "tbbHVit787h", "value": "Gihundwe / Kamembe" }, { "attribute": "AZg4GQtPvC1", "value": "Nkurunziza / Gihundwe" }, { "attribute": "oCAIvI4WqJK", "value": "Male" }, { "attribute": "H0LzJzW2LeM", "value": "1971-01-01" }, { "attribute": "L4EqOsk6Xk8", "value": 52 } ], "enrollments": [ { "orgUnit": "P22YvBU18tV", "program": "L4DxA2Y3mqQ", "enrollmentDate": "2023-07-10T06:48:32", "incidentDate": "2023-07-10T06:48:32", "events": [ { "programStage": "p4zqvI7sJX4", "eventDate": "2023-07-10T06:48:32", "dataValues": [ { "dataElement": "Raiv3NagZiJ", "value": 173.0 }, { "dataElement": "B8gSDW7bnh0", "value": 68.0 }, { "dataElement": "IxEwYiq1FTq", "value": 161.0 }, { "dataElement": "yNhtHKtKkO1", "value": 106.0 }, { "dataElement": "l0DSadolzje", "value": 83.0 }, { "dataElement": "BCTuQ3xPYet", "value": 13467 }, { "dataElement": "bJhWzKtqnar", "value": 98.0 } ] } ] } ] }" you need to find a correct way to extract what represent the tracked entity and what represents the events. can you please fix that method
    
    ASSISTANT: To fix the `process_patient_files` method in `src/connectors/dhis2_connector.py`, we need to modify the way we extract the tracked entity instance data and the events from the patient data.
    
    From the provided JSON structure, it seems that the tracked entity instance data is the entire JSON object, and the events are nested under the 'enrollments' key.
    
    Here are the steps to make the changes:
    
    1. Replace the line where we extract the tracked entity instance data to assign the entire patient data to it.
    2. Replace the loop where we post the event data to loop through the 'enrollments' and 'events' keys.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/connectors/dhis2_connector.py
    ```python
    <<<<<<< SEARCH
    tracked_entity_instance_data = patient_data.get('trackedEntityInstance')
    =======
    tracked_entity_instance_data = patient_data
    >>>>>>> REPLACE
    ```
    
    src/connectors/dhis2_connector.py
    ```python
    <<<<<<< SEARCH
    for enrollment in patient_data.get('enrollments', []):
        for event in enrollment.get('events', []):
            event['trackedEntityInstance'] = entity_id
            logging.info(f"Posting event data: {event}")
            self.make_api_call('events', method='POST', data=event)
    =======
    for enrollment in tracked_entity_instance_data.get('enrollments', []):
        for event in enrollment.get('events', []):
            event['trackedEntityInstance'] = entity_id
            logging.info(f"Posting event data: {json.dumps(event, indent=4)}")
            self.make_api_call('events', method='POST', data=event)
    >>>>>>> REPLACE
    ```
    
    These changes should correctly extract the tracked entity instance data and the events from the patient data.

--- src/connectors/dhis2_connector.py
+++ src/connectors/dhis2_connector.py
@@ -22,16 +22,16 @@ class DHIS2Connector:
             # under a key that is not just 'trackedEntityType'. We need to find the correct key or construct
             # the full JSON object if necessary. For this example, let's assume the full data is under the key
             # 'trackedEntityInstance'.
-            tracked_entity_instance_data = patient_data.get('trackedEntityInstance')
+            tracked_entity_instance_data = patient_data
             if tracked_entity_instance_data:
                 logging.info(f"Posting tracked entity instance data: {json.dumps(tracked_entity_instance_data, indent=4)}")
                 response = self.make_api_call('trackedEntityInstances', method='POST', data=tracked_entity_instance_data)
                 if response and 'response' in response and 'importSummaries' in response['response']:
                     entity_id = response['response']['importSummaries'][0]['reference']
-                    for enrollment in patient_data.get('enrollments', []):
+                    for enrollment in tracked_entity_instance_data.get('enrollments', []):
                         for event in enrollment.get('events', []):
                             event['trackedEntityInstance'] = entity_id
-                            logging.info(f"Posting event data: {event}")
+                            logging.info(f"Posting event data: {json.dumps(event, indent=4)}")
                             self.make_api_call('events', method='POST', data=event)
                     new_filename = f"{entity_id}_{filename}"
                     os.rename(file_path, os.path.join(directory, new_filename))
